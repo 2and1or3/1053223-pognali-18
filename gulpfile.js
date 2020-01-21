84c5bc296fe4afb418c22951213bf32ba040c6bd
2and1or3 <2and1or3@gmail.com>
2020-01-21 21:02:21 +0300

    Оптимизация шрифтов

--- gulpfile.js
+++ gulpfile.js
@@ -90,7 +90,8 @@ gulp.task("clean", function() {
 gulp.task("copy", function() {
   return gulp
     .src([
-      "source/fonts/**/*.{woff, woff2}",
+      "source/fonts/**/*.woff",
+      "source/fonts/**/*.woff2",
       "source/img/**",
       "source/js/**",
       "source/*.ico"
